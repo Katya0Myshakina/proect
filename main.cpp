7fc69dbe8400e526cfaa0395778a1bacb4549eaf
Katya <katya-myshakina@mail.ru>
2024-12-24 08:49:47 +0300

    добавление суммы

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 //программа является учебной
 int main()
 {
-   
+     int a, b;
+
+    std::cout << "Введите два числа: ";
+    std::cin >> a >> b;
+    std::cout << "Сумма чисел: " << a + b << std::endl;
+
     return 0;
 }
